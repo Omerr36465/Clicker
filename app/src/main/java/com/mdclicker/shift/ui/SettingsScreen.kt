bcb5f5f68df5fd72a227dc2e9564e382a49a6a48
Omerr36465 <151486772+Omerr36465@users.noreply.github.com>
2026-05-29 08:04:07 +0300

    Update SettingsScreen.kt

--- app/src/main/java/com/mdclicker/shift/ui/SettingsScreen.kt
+++ app/src/main/java/com/mdclicker/shift/ui/SettingsScreen.kt
@@ -1,7 +1,7 @@
 package com.mdclicker.shift.ui
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Divider //  أضف هذا السطر
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
