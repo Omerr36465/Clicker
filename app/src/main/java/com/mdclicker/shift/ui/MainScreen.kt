0ba906aea44b23ddd36707cd541b1f4b701c3940
Omerr36465 <151486772+Omerr36465@users.noreply.github.com>
2026-05-29 08:03:21 +0300

    Replace HorizontalDivider with Divider import

--- app/src/main/java/com/mdclicker/shift/ui/MainScreen.kt
+++ app/src/main/java/com/mdclicker/shift/ui/MainScreen.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Divider //  أضف هذا السطر
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
